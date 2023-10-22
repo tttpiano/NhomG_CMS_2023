83e3e058694c48908c9c6824ee54504dae1e9876
VietThang <21211tt3051@mail.tdc.edu.vn>
2023-10-22 23:18:51 +0700

    commit module8 by VietThang

--- wp-content/themes/twentytwentyone/comments.php
+++ wp-content/themes/twentytwentyone/comments.php
@@ -82,7 +82,7 @@ $twenty_twenty_one_comment_count = get_comments_number();
 	<?php
 	comment_form(
 		array(
-			'title_reply'        => esc_html__( 'Leave a comment', 'twentytwentyone' ),
+			'title_reply'        => esc_html__( '', 'twentytwentyone' ),
 			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
 			'title_reply_after'  => '</h2>',
 		)
