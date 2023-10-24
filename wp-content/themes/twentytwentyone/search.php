1ed383b7879bd6e9cf52b756113f3ab1a5729510
Manh Ho <honguyenmanh120403@gmail.com>
2023-10-24 13:37:06 +0700

    update moudle_13

--- wp-content/themes/twentytwentyone/search.php
+++ wp-content/themes/twentytwentyone/search.php
@@ -1,9 +1,5 @@
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <style>
     body {
 
@@ -33,10 +29,7 @@
         box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
     }
 
-    .top_news_block_thumb img {
-        max-width: 100%;
-        width: 100%;
-    }
+   
 
     .top_news_block_desc {
         font-size: 0.9em;
@@ -74,6 +67,45 @@
     .shortdesc {
         border-left: 1px solid #666;
     }
+
+    ul.timeline {
+        list-style-type: none;
+        position: relative;
+    }
+    .top_news_block_thumb img {
+   
+    width: unset !important;
+    height: unset !important;
+}
+
+    ul.timeline:before {
+        content: ' ';
+        background: #d4d9df;
+        display: inline-block;
+        position: absolute;
+        left: 29px;
+        width: 2px;
+        height: 100%;
+        z-index: 400;
+    }
+
+    ul.timeline>li {
+        margin: 20px 0;
+        padding-left: 20px;
+    }
+
+    ul.timeline>li:before {
+        content: ' ';
+        background: white;
+        display: inline-block;
+        position: absolute;
+        border-radius: 50%;
+        border: 3px solid #22c0e8;
+        left: 20px;
+        width: 20px;
+        height: 20px;
+        z-index: 400;
+    }
 </style>
 <?php
 $months = array(
@@ -93,49 +125,68 @@ $months = array(
 get_header();
 
 if (have_posts()) {
-    ?>
+?>
     <?php get_template_part('template-parts/content/content-none'); ?>
-    <div class="container">
-        <header class="page-header alignwide">
-            <!--            <h1 class="page-title">-->
-            <!--                --><?php
-            //                printf(
-            //                /* translators: %s: Search term. */
-            //                    esc_html__('Results for "%s"', 'twentytwentyone'),
-            //                    '<span class="page-description search-term">' . esc_html(get_search_query()) . '</span>'
-            //                );
-            //                ?>
-            <!--            </h1>-->
-        </header><!-- .page-header -->
-        <!--        <div class="search-result-count default-max-width">-->
-        <!--            --><?php
-        while (have_posts()) {
-            the_post();
-            // Hiển thị nội dung của kết quả tìm kiếm
-        }
-        //            printf(
-        //                esc_html(
-        //                /* translators: %d: The number of search results. */
-        //                    _n(
-        //                        'We found %d result for your search.',
-        //                        'We found %d results for your search.',
-        //                        (int) $wp_query->found_posts,
-        //                        'twentytwentyone'
-        //                    )
-        //                ),
-        //                (int) $wp_query->found_posts
-        //            );
-        //            ?>
-        <!--        </div> .search-result-count -->
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-md-3">
+
+
+
+
+                <div class="row justify-content-center">
+                    <h1>Trang mới nhất</h1>
+                  
+                    
+                    <div class="col-12">
+                        <!-- Tin tức thứ ba (cấu trúc tương tự tin tức đầu tiên) -->
+                        <div class="card">
+                        <?php
+        $args = array(
+            'post_type' => 'post',  // Loại bài viết là "post" (bài viết thông thường).
+            'posts_per_page' => 3,  // Số bài viết muốn hiển thị (1 bài viết mới nhất).
+            'order' => 'DESC',     // Sắp xếp theo thứ tự giảm dần (từ mới nhất đến cũ nhất).
+        );
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) :
+            while ($query->have_posts()) : $query->the_post();
+        ?>
+
+        <div class="col-12">
+            <div class="card">
+                <?php if (has_post_thumbnail()) : ?>
+                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="card-img-top" alt="...">
+                <?php endif; ?>
+                <div class="card-body">
+                    <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p class="card-text"><?php the_excerpt(); ?></p>
+                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Đọc thêm</a>
+                </div>
+            </div>
+        </div>
 
         <?php
-        // Start the Loop.
-        while (have_posts()) {
-            the_post();
-            ?>
-            <div class="row justify-content-center">
-                <div  class="col-md-3"></div>
-                <div class="col-md-6">
+            endwhile;
+            wp_reset_postdata();
+        endif;
+        ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <?php
+                // Start the Loop.
+                while (have_posts()) {
+                    the_post();
+                ?>
+
+
+
+
                     <div class="list_new_view">
                         <div class="row">
                             <div class="col-md-5">
@@ -152,7 +203,7 @@ if (have_posts()) {
                                 <div class="row">
                                     <div class="col-md-4 col-xs-3 topnewstime">
                                         <span class="topnewsdate"><?php echo get_the_time('d'); ?></span><br>
-                                        <span class="topnewsmonth" ><?php echo $months[get_the_time('F')]; ?></span><br>
+                                        <span class="topnewsmonth"><?php echo $months[get_the_time('F')]; ?></span><br>
                                     </div>
                                     <div class="col-md-8 col-xs-9 shortdesc">
                                         <h4 class="h4">
@@ -174,16 +225,65 @@ if (have_posts()) {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-3"></div>
-            </div>
-            <?php
-        } // End the loop.
-        ?>
 
+
+
+
+                <?php
+                } // End the loop.
+                ?>
+            </div>
+            <div class="col-md-3">
+
+            </div>
+
+        </div>
+    </div>
+    <div class="col-md-6 offset-md-3">
+        <h4>Latest News</h4>
+        <ul class="timeline">
+
+            <?php
+
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => -1
+            );
+
+            $query = new WP_Query($args);
+
+            if ($query->have_posts()) :
+                while ($query->have_posts()) : $query->the_post();
+
+            ?>
+                    <li>
+                        <a target="_blank" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        <a href="#" class="float-right"><?php echo get_the_date('d'); ?> <?php echo get_the_date('M'); ?>, <?php echo get_the_date('Y'); ?></a>
+                        <p>
+                            <?php
+                            $excerpt = get_the_excerpt();
+                            $excerpt = wp_trim_words($excerpt, 50, '.');
+                            echo $excerpt;
+
+
+                            ?>
+                            <a class="abc" href="<?php the_permalink(); ?>">[...]</a>
+
+                        </p>
+                    </li>
+
+            <?php
+                endwhile;
+                wp_reset_postdata();
+            else :
+                echo 'No posts found.';
+            endif;
+
+            ?>
+        </ul>
     </div>
 
-    <?php
+<?php
     // Previous/next page navigation.
     twenty_twenty_one_the_posts_navigation();
 } else {
